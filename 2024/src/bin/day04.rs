a22b27935285c8adbbc657dad2de4ecad077151f
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-02-05 16:24:43 +0000

    Move rotate() funcs to utils

--- 2024/src/bin/day04.rs
+++ 2024/src/bin/day04.rs
@@ -9,52 +9,6 @@ fn main() {
     println!("Part 2: {}", part_two(&input));
 }
 
-// Rotate the matrix 90 degrees clockwise, so that we can check the columns.
-fn rotate_90(cw: &[Vec<char>]) -> Vec<Vec<char>> {
-    let mut ccw = vec![vec![' '; cw.len()]; cw[0].len()];
-    for i in 0..cw.len() {
-        for j in 0..cw[0].len() {
-            ccw[j][cw.len() - i - 1] = cw[i][j];
-        }
-    }
-    ccw
-}
-
-// Rotate the matrix 45 degrees clockwise, so that we can check the diagonals.
-fn rotate_45(cw: &[Vec<char>]) -> Vec<Vec<char>> {
-    let mut ccw = Vec::new();
-
-    // starting at the first col, iterate down the rows, going up and right at 45 degrees
-    for i in 0..cw.len() {
-        let mut x = i; // rows 0 to n
-        let mut y = 0; // first column
-        let mut diagonal = Vec::new();
-        diagonal.push(cw[x][y]);
-        while x > 0 && y < cw[0].len() - 1 {
-            x -= 1;  // up
-            y += 1;  // right
-            diagonal.push(cw[x][y]);
-        }
-        ccw.push(diagonal);
-    }
-
-    // starting at the last row, iterate across the columns, from col 1 (not 0) going up and right at 45 degrees
-    for j in 1..cw[0].len() {
-        let mut x = cw.len() - 1;  // last row
-        let mut y = j;  // columns 1 to n
-        let mut diagonal = Vec::new();
-        diagonal.push(cw[x][y]);
-        while x > 0 && y < cw[0].len() - 1 {
-            x -= 1;  // up
-            y += 1;  // right
-            diagonal.push(cw[x][y]);
-        }
-        ccw.push(diagonal);
-    }
-
-    ccw
-}
-
 fn part_one(s: &str) -> usize {
     let mut cw = s
         .lines()
@@ -62,12 +16,12 @@ fn part_one(s: &str) -> usize {
         .collect::<Vec<Vec<char>>>();
     let mut xmases = 0;
     for _ in 0..4 {
-        let ninety = rotate_90(&cw);
+        let ninety = utils::rotate_90(&cw);
         for row in &ninety {
             let s = row.iter().collect::<String>();
             xmases += s.matches("XMAS").count();
         }
-        let fortyfive = rotate_45(&cw);
+        let fortyfive = utils::rotate_45(&cw);
         for row in &fortyfive {
             let s = row.iter().collect::<String>();
             xmases += s.matches("XMAS").count();
@@ -86,27 +40,6 @@ mod tests {
     use crate::utils::example_input;
 
     use super::*;
-
-    #[test]
-    fn test_rotate_90() {
-        let cw = vec![vec!['a', 'b', 'c'], vec!['d', 'e', 'f']];
-        let ccw = vec![vec!['d', 'a'], vec!['e', 'b'], vec!['f', 'c']];
-        assert_eq!(rotate_90(&cw), ccw);
-    }
-
-    #[test]
-    fn test_rotate_45_one() {
-        let orig = vec![vec!['a', 'b', 'c'], vec!['d', 'e', 'f']];
-
-        let rotated = vec![vec!['a'], vec!['d', 'b'], vec!['e', 'c'], vec!['f']];
-        assert_eq!(rotate_45(&orig), rotated);
-    }
-
-    #[test]
-    fn test_rotate_45_two() {
-        let orig = vec![vec!['a'], vec!['d']];
-        assert_eq!(rotate_45(&orig), orig);
-    }
 
     #[test]
     fn test_part_one() {
--- 2024/src/bin/utils.rs
+++ 2024/src/bin/utils.rs
@@ -19,4 +19,78 @@ pub(crate) fn problem_input(day: &str) -> String {
 }
 
 #[allow(dead_code)]
+// Rotate the matrix 90 degrees clockwise, so that we can check the columns.
+pub(crate) fn rotate_90(cw: &[Vec<char>]) -> Vec<Vec<char>> {
+    let mut ccw = vec![vec![' '; cw.len()]; cw[0].len()];
+    for i in 0..cw.len() {
+        for j in 0..cw[0].len() {
+            ccw[j][cw.len() - i - 1] = cw[i][j];
+        }
+    }
+    ccw
+}
+
+#[allow(dead_code)]
+// Rotate the matrix 45 degrees clockwise, so that we can check the diagonals.
+pub(crate) fn rotate_45(cw: &[Vec<char>]) -> Vec<Vec<char>> {
+    let mut ccw = Vec::new();
+
+    // starting at the first col, iterate down the rows, going up and right at 45 degrees
+    for i in 0..cw.len() {
+        let mut x = i; // rows 0 to n
+        let mut y = 0; // first column
+        let mut diagonal = Vec::new();
+        diagonal.push(cw[x][y]);
+        while x > 0 && y < cw[0].len() - 1 {
+            x -= 1; // up
+            y += 1; // right
+            diagonal.push(cw[x][y]);
+        }
+        ccw.push(diagonal);
+    }
+
+    // starting at the last row, iterate across the columns, from col 1 (not 0) going up and right at 45 degrees
+    for j in 1..cw[0].len() {
+        let mut x = cw.len() - 1; // last row
+        let mut y = j; // columns 1 to n
+        let mut diagonal = Vec::new();
+        diagonal.push(cw[x][y]);
+        while x > 0 && y < cw[0].len() - 1 {
+            x -= 1; // up
+            y += 1; // right
+            diagonal.push(cw[x][y]);
+        }
+        ccw.push(diagonal);
+    }
+
+    ccw
+}
+
+#[allow(dead_code)]
 fn main() {}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_rotate_90() {
+        let cw = vec![vec!['a', 'b', 'c'], vec!['d', 'e', 'f']];
+        let ccw = vec![vec!['d', 'a'], vec!['e', 'b'], vec!['f', 'c']];
+        assert_eq!(rotate_90(&cw), ccw);
+    }
+
+    #[test]
+    fn test_rotate_45_one() {
+        let orig = vec![vec!['a', 'b', 'c'], vec!['d', 'e', 'f']];
+
+        let rotated = vec![vec!['a'], vec!['d', 'b'], vec!['e', 'c'], vec!['f']];
+        assert_eq!(rotate_45(&orig), rotated);
+    }
+    #[test]
+    fn test_rotate_45_two() {
+        let orig = vec![vec!['a'], vec!['d']];
+        assert_eq!(rotate_45(&orig), orig);
+    }
+}
