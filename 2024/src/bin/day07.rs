732e0c2ad561fde03862631ce7204f60978ed3fb
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-04-07 16:49:31 +0100

    Day 7, part 2

--- 2024/src/bin/day07.rs
+++ 2024/src/bin/day07.rs
@@ -118,8 +118,105 @@ fn can_form(test_val: u64, mut nums: VecDeque<u64>) -> (bool, String) {
     return (false, "".to_owned());
 }
 
+fn can_form_two(test_val: u64, mut nums: VecDeque<u64>) -> (bool, String) {
+    let first = nums.pop_back().unwrap();
+    // Our stopping condition.
+    if nums.len() == 0 {
+        if first == test_val {
+            return (true, "".to_owned());
+        } else {
+            return (false, "".to_owned());
+        }
+    }
+
+    if test_val >= first {
+        let add = can_form_two(test_val - first, nums.clone());
+        if add.0 {
+            return (true, add.1 + "+");
+        }
+    }
+
+    if test_val % first == 0 {
+        let mul = can_form_two(test_val / first, nums.clone());
+        if mul.0 {
+            return (true, mul.1 + "*");
+        }
+    }
+
+    // We know we have at least one more number.
+    // let last = nums[nums.len() - 1];
+    // let combined = last.to_string() + first.to_string().as_str();
+    // if last.to_string() + first.to_string().as_str() == test_val.to_string() {
+    // return (true, "||".to_owned());
+    // }
+    let first_string = first.to_string();
+    let first_string_len = first_string.len();
+    let test_val_string = test_val.to_string();
+    let test_val_string_len = test_val_string.len();
+    if test_val_string_len > first_string_len && test_val_string.ends_with(first_string.as_str()) {
+        let index = test_val_string.len() - first_string.len();
+        let concatt = can_form_two(
+            test_val_string[..index].parse::<u64>().unwrap(),
+            nums.clone(),
+        );
+        if concatt.0 {
+            return (true, concatt.1 + "|");
+        }
+    }
+
+    return (false, "".to_owned());
+}
+
 fn part_two(s: &str) -> u64 {
-    2
+    let myvec = s
+        .lines()
+        .map(|line| {
+            let mut parts = line.split(":");
+            let test_val: u64 = parts.next().unwrap().parse().unwrap();
+            let nums: VecDeque<u64> = parts
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            (test_val, nums)
+        })
+        .collect::<Vec<_>>();
+    let mut total = 0;
+    for (test_val, nums) in myvec.iter() {
+        let test_val_val = *test_val;
+        let debug_nums = nums.clone();
+        let canform = can_form_two(*test_val, nums.clone());
+        if canform.0 {
+            let mut clone = nums.clone();
+            let mut subtotal = clone.pop_front().unwrap();
+            for c in canform.1.chars() {
+                if c == '+' {
+                    subtotal = subtotal + clone.pop_front().unwrap();
+                } else if c == '*' {
+                    subtotal = subtotal * clone.pop_front().unwrap();
+                } else if c == '|' {
+                    subtotal = (subtotal.to_string()
+                        + clone.pop_front().unwrap().to_string().as_str())
+                    .parse::<u64>()
+                    .unwrap();
+                } else {
+                    panic!("Unexpected character: {}", c);
+                }
+            }
+            if subtotal != *test_val {
+                // can_form(*test_val, nums.clone());
+                panic!(
+                    "Subtotal {} does not equal test value {}",
+                    subtotal, test_val
+                );
+                // let a = 1;
+            }
+
+            total += test_val;
+        }
+    }
+    total
 }
 
 #[cfg(test)]
@@ -133,7 +230,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&example_input("07"));
-        assert_eq!(result, 3749);
+        assert_eq!(result, 3_749);
     }
 
     #[test]
@@ -228,8 +325,17 @@ mod tests {
     }
 
     #[test]
+    fn test_can_form_two() {
+        let result = can_form_two(156, vec![15, 6].into());
+        assert_eq!(result.0, true);
+
+        let result = can_form_two(312, vec![15, 6, 2].into());
+        assert_eq!(result.0, true);
+    }
+
+    #[test]
     fn test_part_two() {
-        let result = part_two(&example_input("00"));
-        assert_eq!(result, 0);
+        let result = part_two(&example_input("07"));
+        assert_eq!(result, 11_387);
     }
 }
